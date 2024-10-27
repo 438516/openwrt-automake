b0767b69ccfc8e85015f54ebf1da5805f61b383e
438516 <70022856+438516@users.noreply.github.com>
2024-10-27 20:47:48 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,34 +1,14 @@
-#!/bin/bash
-#
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
-#
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 
-# Uncomment a feed source
-#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-# Add a feed source
-#echo 'src-git opentopd  https://github.com/sirpdboy/sirpdboy-package' >>feeds.conf.default
-#echo 'src-git oceanr https://github.com/oceanromain/openwrt-packages.git' >>feeds.conf.default
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages.git' >>feeds.conf.default
-
-echo 'src-git small https://github.com/kenzok8/small.git' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages.git' >>feeds.conf.default
-#echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >>feeds.conf.default
-#echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >>feeds.conf.default
-#echo 'src-git ddns_go https://github.com/sirpdboy/luci-app-ddns-go' >>feeds.conf.default
-#echo 'src-git autocore https://github.com/oceanromain/myautocore package/myautocore' >>feeds.conf.default
-echo 'src-git oceanweb https://github.com/oceanromain/my-openwrt-package.git' >>feeds.conf.default
 echo "src-git mihomo https://github.com/morytyann/OpenWrt-mihomo.git;main" >> "feeds.conf.default"
-#echo 'src-git small_all https://github.com/kenzok8/small-package.git' >> feeds.conf.default
-#20241025
-#echo 'src-git small_all https://github.com/kenzok8/small-package.git' >> feeds.conf.default
-#echo 'src-git nxx https://github.com/NueXini/NueXini_Packages.git' >> feeds.conf.default
+
+git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
+
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
